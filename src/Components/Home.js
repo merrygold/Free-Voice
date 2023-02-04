92332afee3ba2678e88f2277b500e8d137a0c941
merrygold <merrygold643@gmail.com>
2023-02-05 00:30:57 +0500

    Home and Syndicate almost Done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,33 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import "../Components/Home.css";
-import abi from "../lib/abi.json"
+import abi from "../lib/abi.json";
 import Web3 from "web3";
+// import File from "../images/icons/file2.png";
+import File from "../images/icons/file2.png";
 import { Link } from "react-router-dom";
+import lighthouse from "@lighthouse-web3/sdk";
+import { useAccount } from "wagmi";
 const Home = () => {
   const [postContent, setPostContent] = useState("");
   const [Syndicate, setSyndicate] = useState([]);
-  const [Post, setPost] = useState("");
-
+  const [Posts, setPosts] = useState("");
+  const {address} = useAccount()
   // Create a provider using the RPC link
-  const RPC = "https://api.hyperspace.node.glif.io/rpc/v1"
+
+  // ** API GOD_FATHER
+  const API = "1e8ba22b-e3c5-4335-aadd-79b5f614ce0f";
+
+  const RPC = "https://api.hyperspace.node.glif.io/rpc/v1";
 
   const provider = new ethers.providers.JsonRpcProvider(RPC);
   const web3 = new Web3(new Web3.providers.HttpProvider(RPC));
 
-  const ABI = abi
+  const ABI = abi;
 
-  const ContractAddress = "0x3Fe2d23543B3cb228e2a0b14FAf055b583EA5c99"
+  const ContractAddress = "0x9528f220eBBC5770FAAc338d5018d871708e6EDb";
 
-  const mainContract = new ethers.Contract(ContractAddress, ABI, provider)
+  const mainContract = new ethers.Contract(ContractAddress, ABI, provider);
 
+  // * Utility Functions
   // * BigNumber to JS Number
   function bigToNum(_value) {
     const bigNumber = ethers.BigNumber.from(_value);
     return bigNumber.toNumber();
   }
 
-
+  // * Remove Duplicate objects from an array
   function removeDuplicateObjects(array) {
     const seen = new Set();
     return array.filter((item) => {
@@ -42,60 +51,255 @@ const Home = () => {
 
 
 
-  useEffect(() => {
-
-    async function main() {
-      await getAllSyndicates()
-    }
-
-    main()
-  }, []);
-
-
-
-
-
+  // * Get All the Sundicates from the Smart Contract
   async function getAllSyndicates() {
     const contract = new web3.eth.Contract(ABI, ContractAddress);
 
     // Parse events from contract
-    const syndicates = await contract.getPastEvents('EventCreateSyndicate', {
-      fromBlock: 50000,
-      toBlock: 'latest'
-    },
+    const syndicates = await contract.getPastEvents(
+      "EventCreateSyndicate",
+      {
+        fromBlock: 50000,
+        toBlock: "latest",
+      },
       function (error, eventsArray) {
         if (error) {
-          console.log("there is an error")
+          console.log("there is an error");
         }
-        return eventsArray
+        return eventsArray;
       }
     );
-    const filterSyndicates = removeDuplicateObjects(syndicates)
-    setSyndicate(filterSyndicates)
-    console.log(filterSyndicates)
-
+    const filterSyndicates = removeDuplicateObjects(syndicates);
+    setSyndicate(filterSyndicates);
+    console.log(filterSyndicates);
   }
 
+  // * State Hooks for Creating a Syndicate
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [NFTname, setNFTname] = useState("");
+  const [NFTsymbol, setNFTsymbol] = useState("");
+
+  // * Input event handlers
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleDescChange = (event) => {
+    setDesc(event.target.value);
+  };
+  const handleNFTnameChange = (event) => {
+    setNFTname(event.target.value);
+  };
+  const handleNFTsymbolChange = (event) => {
+    setNFTsymbol(event.target.value);
+  };
+
+  // * Create a New Syndicate
+  async function createSyndicate(
+    _name,
+    _desc,
+    _NFTname,
+    _NFTsymbol,
+    _imageHash
+  ) {
+    const signer = new ethers.providers.Web3Provider(
+      window.ethereum
+    ).getSigner();
+    const signContract = new ethers.Contract(ContractAddress, ABI, signer);
+    const newSyndicate = await signContract.createSyndicate(
+      _name,
+      _desc,
+      _NFTname,
+      _NFTsymbol,
+      _imageHash
+    );
+
+    await newSyndicate.wait();
+    console.log("New Syndicate Created");
+  }
+
+  // * Button onClick "Create New Syndicate"
+  const createSyndicateButton = async () => {
+    const imageCid = await imageUploadSyndicate();
+    await createSyndicate(name, desc, NFTname, NFTsymbol, imageCid);
+  };
+
+  // * Image Upload function for the Syndicate
+  const [simpleFile, setSimpleFile] = useState(null);
+  const [inputRefSimple, setInputRefSimple] = useState(null);
+
+  const progressCallback = (progressData) => {
+    let percentageDone =
+      100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+    console.log(percentageDone);
+  };
+  const imageUploadSyndicate = async () => {
+    // * Upload File to lighthouse
+    const output = await lighthouse.upload(simpleFile, API, progressCallback);
+    // * Get Cid of the file
+    const imageCid = output.data.Hash;
+    return imageCid;
+  };
+
+  // * Get that Uploaded Simple file and then saving it to a state so we can use later
+  const handleSimpleFile = (e) => {
+    console.log(e);
+    setSimpleFile(e);
+  };
+
+  const handleImageClick = () => {
+    inputRefSimple.click();
+  };
+
+  // * Get all the posts of the syndicates user joined on the Home page
+
+  const[displayPosts , setDisplayPosts] = useState('')
+
+  async function getHomePosts(){
+
+    // * Create Web3 Provider
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC));
+
+    // * Create Web3 Contract
+    const contract = new web3.eth.Contract(ABI, ContractAddress);
+
+    //* Get All User Syndicates
+    const userSyndicatesDouble = await contract.getPastEvents('EventJoinSyndicate', {
+        fromBlock: 50000,
+        toBlock: 'latest',
+    },
+    function(error, eventsArray) {
+      return eventsArray
+    }
+    );
+
+    // * Get Current User Joined Syndicates
+    const userSyndicatesArray = removeDuplicateObjects(userSyndicatesDouble)
+    const userSyndicates = []
+
+    for (let i = 0; i < userSyndicatesArray.length; i++) {
+        const currentEvent = userSyndicatesArray[i]
+        // * If Event Match User joining
+        if(currentEvent.returnValues._member == address) {
+            // * Push syndicate Id to userSyndicates
+            userSyndicates.push(currentEvent.returnValues.id)
+        }
+    }
+
+    // * 2. Get All posts for User Syndicates
+    // * Get all posts
+    const syndicatesPostsDouble = await contract.getPastEvents('PostCreated', {
+        fromBlock: 50000,
+        toBlock: 'latest',
+    },
+    function(error, eventsArray) {
+      return eventsArray
+    }
+    );
+
+    const syndicatesPostsArray = removeDuplicateObjects(syndicatesPostsDouble)
+
+    const userHomePosts = []
+
+    // * Get only User Syndicates Post
+    for (let i = 0; i < syndicatesPostsArray.length; i++) {
+        const currentPost = syndicatesPostsArray[i]
+        const currentPostSyndicateId = currentPost.returnValues.syndicateId
+
+        // * If Posts User Ids is found inside UserSyndicates array
+        // * Push the post into user Home Posts
+        if(userSyndicates.includes(currentPostSyndicateId)) {
+            userHomePosts.push(currentPost)
+        }
+    }
+
+    // * return all User Home Posts
+    setDisplayPosts(userHomePosts)
+    console.log("These are the Posts")
+    console.log(userHomePosts)
+    return userHomePosts;
+    
+
+}
 
 
 
-  // const handlePostContentChange = (event) => {
-  //   setPostContent(event.target.value);
-  // };
+    // * Main State Loader
+    useEffect(() => {
+      async function main() {
+        await getAllSyndicates();
+        await getHomePosts()
+      }
+  
+      main();
+    }, [address]);
 
-  // const handlePostSubmit = async (event) => {
-  //   event.preventDefault();
+
 
 
   return (
     <>
       <div>Posts will Display here</div>
 
+      {/*Image Upload*/}
+      <form className="files-upload">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="desc">Description:</label>
+          <input
+            type="text"
+            id="desc"
+            value={desc}
+            onChange={handleDescChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="NFTname">NFT Name:</label>
+          <input
+            type="text"
+            id="NFTname"
+            value={NFTname}
+            onChange={handleNFTnameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="NFTsymbol">NFT Symbol:</label>
+          <input
+            type="text"
+            id="NFTsymbol"
+            value={NFTsymbol}
+            onChange={handleNFTsymbolChange}
+          />
+        </div>
+        <img className="file-icon" src={File} onClick={handleImageClick} />
+        <input
+          type="file"
+          ref={(input) => setInputRefSimple(input)}
+          onChange={handleSimpleFile}
+          style={{ display: "none" }}
+        />
+        {!simpleFile && (
+          <p className="box-text">Upload files (Simple Upload) </p>
+        )}
+        {simpleFile && (
+          <p className="box-text">{simpleFile.target.files[0].name}</p>
+        )}
+
+        <a onClick={createSyndicateButton} className="upload-btn">
+          Create Syndicate
+        </a>
+      </form>
+
       <div className="Syndicate">Syndicate Array</div>
-
-
-
-      {Syndicate.map(item => (
+      {Syndicate.map((item) => (
         <ul key={item.returnValues.syndicateCount} className="Syndicate">
           <Link
             to={`/Syndicates/${item.returnValues.syndicateCount}`}
@@ -107,21 +311,9 @@ const Home = () => {
             <li>{item.blockHash}</li>
             <li>{item.returnValues.syndicateCount}</li>
           </Link>
-
         </ul>
       ))}
-
-
     </>
-    // <form onSubmit={handlePostSubmit}>
-    //   <textarea
-    //     value={postContent}
-    //     onChange={handlePostContentChange}
-    //     placeholder="What's happening?"
-    //     rows="4"
-    //   />
-    //   <button type="submit">Tweet</button>
-    // </form>
   );
 };
 
--- src/Components/Syndicate.js
+++ src/Components/Syndicate.js
@@ -1,90 +1,266 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
-import abi from "../lib/abi.json"
+import abi from "../lib/abi.json";
+import nftAbi from "../lib/nftAbi.json"
 import Web3 from "web3";
 import { ethers } from "ethers";
-import './Syndicate.css';
-import { useSigner } from 'wagmi'
+import lighthouse from "@lighthouse-web3/sdk";
+import File from "../images/icons/file2.png";
+import "./Syndicate.css";
+import { useAccount } from "wagmi";
 
 function Syndicate() {
 
+  // ? CONTRACT BOILERPLATE
+  // ** API GOD_FATHER
+  const API = "1e8ba22b-e3c5-4335-aadd-79b5f614ce0f"
 
-  const [src, setSrc] = useState(null);
-  const IPFS = "https://ipfs.moralis.io:2053/ipfs/QmQmWq9mbB4GaiDVhbK6mbzhmc5ZTAEkJssRTYvht7eqdb";
-
-  const fetchImage = async () => {
-    try {
-      const res = await fetch(IPFS);
-      const blob = await res.blob();
-      setSrc(URL.createObjectURL(blob));
-    } catch (error) {
-
-    }
-  };
-
-
-
-  // Create a provider using the RPC link
+  //* Create a provider using the RPC link
   const RPC = "https://api.hyperspace.node.glif.io/rpc/v1";
   const provider = new ethers.providers.JsonRpcProvider(RPC);
   const web3 = new Web3(new Web3.providers.HttpProvider(RPC));
 
-  const ABI = abi
+  const ABI = abi;
 
-  const ContractAddress = "0x3Fe2d23543B3cb228e2a0b14FAf055b583EA5c99"
+  const ContractAddress = "0x9528f220eBBC5770FAAc338d5018d871708e6EDb";
 
-  const mainContract = new ethers.Contract(ContractAddress, ABI, provider)
+  const mainContract = new ethers.Contract(ContractAddress, ABI, provider);
+
+  // * State Variables for NFT Contract
+  const NftABI = nftAbi;
+
+// * BigNumber to JS Number
+  function bigToNum(_value) {
+  const bigNumber = ethers.BigNumber.from(_value);
+  return bigNumber.toNumber();
+}
+  // * State Hooks for NFT Dashboard
+  // * Get Current User Address
+  const {address} = useAccount()
+  const [isOwnerNFT, setIsOwnerNFT] = useState(false);
+  const [memberAddress , setMemberAddress] = useState("");
+  const [userBalance , setUserBalance] = useState(0);
 
 
-
-
-  const { state: syndicateDetails } = useLocation();
-  const [syndicate, setSyndicate] = useState()
-  const [description, setDescription] = useState("");
-  const [title, setTitle] = useState("");
-  const [spoiler, setSpoiler] = useState(false);
-  const [original, setOriginal] = useState(false);
-  // const syndicateID = syndicateDetails.id
-
-
-
-  async function getSyndicate() {
-    const syndicate = await mainContract.syndicates(syndicateDetails.id)
-    setSyndicate(syndicate)
-    console.log(syndicate)
+  // * Get isOwner
+  async function getOwner(_nftAddress) {
+    const nftContract = new ethers.Contract(_nftAddress, NftABI, provider);
+    const isOwner = await nftContract.owner()
+    if (isOwner == address) {
+      setIsOwnerNFT(true)
+      const balance = await nftContract.balanceOf(address)
+      setUserBalance(bigToNum(balance))
+      return true
+    }else {
+      setIsOwnerNFT(false)
+      const balance = await nftContract.balanceOf(address)
+      setUserBalance(bigToNum(balance))
+      console.log(bigToNum(balance))
+    }
+ 
   }
 
 
-  async function createTextPost(_description, _title, _isSpoiler, _isOC, _syndicateId) {
+  async function addMember()
+  {
     const signer = new ethers.providers.Web3Provider(
       window.ethereum
     ).getSigner();
-    const signContract = new ethers.Contract(ContractAddress, ABI, signer)
-    const newTextPost = await signContract.uploadTextContent(_description, _title, _isSpoiler, _isOC, _syndicateId);
-    await newTextPost.wait();
-    console.log("New Post Created")
+    const nftContract = new ethers.Contract(syndicate.NftContract, NftABI, signer);
+    
+    const addMemberTransaction = await nftContract.safeMint(memberAddress)
+    await addMemberTransaction.wait()
+    console.log("Minted Successfully")
   }
 
 
-  const createPostButton = async () => {
 
-    await createTextPost(description, title, spoiler, original, syndicateDetails.id);
+  // * State Hooks for Current Syndicate
+  const { state: syndicateId } = useLocation();
+  const [syndicate, setSyndicate] = useState();
+
+  // * Get a Syndicate by Id
+  async function getSyndicate() {
+    const syndicate = await mainContract.syndicates(syndicateId.id);
+    setSyndicate(syndicate);
+    
+    return syndicate;
+    
+  }
+
+  // * State Hooks for CreatePost
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
+  const [spoiler, setSpoiler] = useState(false);
+
+  // * Create A Post in current Syndicate
+  async function createTextPost(
+    _description,
+    _title,
+    _hasImage,
+    _imageHash,
+    _isSpoiler,
+    _syndicateId
+  ) {
+    // * Get Signer
+    const signer = new ethers.providers.Web3Provider(
+      window.ethereum
+    ).getSigner();
+    // * Create Contract Instance
+    const signContract = new ethers.Contract(ContractAddress, ABI, signer);
+
+    // * Create Post
+    const newTextPost = await signContract.uploadTextContent(
+      _description,
+      _title,
+      _hasImage,
+      _imageHash,
+      _isSpoiler,
+      _syndicateId
+    );
+
+    // * wait for transaction to get mined
+    await newTextPost.wait();
+    console.log("New Post Created");
+  }
+
+  const createPostButton = async () => {
+    var imageCid = ""
+    var hasImage = false
+
+    if (simpleFile) {
+      imageCid = await imageUploadPost();
+      hasImage = true
+    }
+
+    await createTextPost(
+      description,
+      title,
+      hasImage,
+      imageCid,
+      spoiler,
+      syndicateId.id
+    );
   };
 
+  // * Image Upload function for the Syndicate
+  const [simpleFile, setSimpleFile] = useState(null);
+  const [inputRefSimple, setInputRefSimple] = useState(null);
+  
+  // * Uplaod to Lighthouse
+  const progressCallback = (progressData) => {
+    let percentageDone =
+      100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+    console.log(percentageDone);
+  };
+
+  // * Upload Image 
+  const imageUploadPost = async() =>{
+    // * Upload File to lighthouse
+    const output = await lighthouse.upload(simpleFile, API, progressCallback);
+    // * Get Cid of the file
+    const imageCid = output.data.Hash
+    return imageCid;
+  }
+
+  // * Handlers for File Upload
+  const handleSimpleFile = (e) => {
+    setSimpleFile(e);
+  };
+  const handleImageClick = () => {
+    inputRefSimple.click();
+  };
+
+  // * Get all the Posts of the Syncdicate
+  const [allSyndicatePosts , setAllSyndicatesPosts] = useState("")
+  // * Remove Duplicate objects from an array
+function removeDuplicateObjects(array) {
+  const seen = new Set();
+  return array.filter((item) => {
+    if (seen.has(item.transactionHash)) {
+      return false;
+    } else {
+      seen.add(item.transactionHash);
+      return true;
+    }
+  });
+}
+  async function getSyndicatePosts() {
+
+    // * Create Web3 Provider
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC));
+
+    // * Create Web3 Contract
+    const contract = new web3.eth.Contract(ABI, ContractAddress);
+
+    // * Get All Posts
+    const syndicatesTotal = await contract.getPastEvents('PostCreated', {
+        fromBlock: 50000,
+        toBlock: 'latest',
+    },
+    function(error, eventsArray) {
+      return eventsArray
+    }
+    );
+
+    // 
+    const syndicates = removeDuplicateObjects(syndicatesTotal)
+
+    const PostsInSyndicate = []
+
+    for (let i = 0; i < syndicates.length; i++) {
+      const currentPost = syndicates[i]
+
+      if (currentPost.returnValues.syndicateId == syndicateId.id) {
+        PostsInSyndicate.push(currentPost)
+      }
+    }
+    PostsInSyndicate.reverse()
+    setAllSyndicatesPosts(PostsInSyndicate)
+    console.log(PostsInSyndicate)
+    return PostsInSyndicate
+}
+
+
+  // * UseEffect for the Whole Statee
   useEffect(() => {
     async function main() {
-      await fetchImage();
-      await getSyndicate()
-    }
-    main()
-    console.log(syndicateDetails.id)
+      
+      const syndicateData = await getSyndicate();
+      // console.log(syndicateData)
 
-  }, []);
+      const isOwner = await getOwner(syndicateData.NftContract)
+
+      const SyndicatePosts = await getSyndicatePosts()
+      // console.log(isOwner)
+    }
+    main();
+  }, [address]);
+
+
+
+ 
+
   return (
-    <div className='coming-soon'>
-      <img src={src} alt="Loading Image" />;
-      {/* <form className="form">
+    <div className="coming-soon">
+      {isOwnerNFT && 
+      <>
+      <div> Syndicate Owner </div>
+      <input
+      type="text"
+      placeholder="Member Address"
+      value={memberAddress}
+      onChange={(e) => setMemberAddress(e.target.value)}
+      required
+       />
+
+      <a onClick={addMember}>Add to Syndicate</a>
+    </>
+    }
+    <div>Balance of NFT : {userBalance}</div>
+      {/* <img src={src} alt="Loading Image" />; */}
+      <form className="form">
         <div className="form-inputs">
           <input
             type="text"
@@ -128,42 +304,32 @@ function Syndicate() {
             False
           </label>
         </div>
-
         <div>
-          <div className="text">Original Content</div>
+          <img className="file-icon" src={File} onClick={handleImageClick} />
+          <input
+            type="file"
+            ref={(input) => setInputRefSimple(input)}
+            onChange={handleSimpleFile}
+            style={{ display: "none" }}
+          />
+          {!simpleFile && (
+            <p className="box-text">Upload files (Simple Upload) </p>
+          )}
+          {simpleFile && (
+            <p className="box-text">{simpleFile.target.files[0].name}</p>
+          )}
         </div>
-        <div className="form-radio-buttons">
-          <label>
-            <input
-              type="radio"
-              name="original"
-              value="true"
-              checked={original === true}
-              onChange={(e) => setOriginal(e.target.value)}
-              required
-            />
-            True
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="original"
-              value="false"
-              checked={original === false}
-              onChange={(e) => setOriginal(e.target.value)}
-              required
-            />
-            False
-          </label>
-
-
+        <div>
+          <div className="text">Contains Image</div>
         </div>
 
-        <a onClick={createPostButton} className="form-button">Post</a>
-      </form> */}
+
+        <a onClick={createPostButton} className="form-button">
+          Post
+        </a>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Syndicate
-
+export default Syndicate;
